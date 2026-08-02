a77adbca9734bbac4437bcc5e2d46f55b0e912cd
Helge Sverre <helge.sverre@gmail.com>
2026-08-03 00:56:40 +0200

    chore: apply cargo fmt

--- src/detect/node.rs
+++ src/detect/node.rs
@@ -1402,8 +1402,9 @@ mod tests {
             ..named.clone()
         };
         #[cfg(windows)]
-        let unnamed_selector =
-            PathBuf::from(".").join(PathBuf::from("apps/web")).into_os_string();
+        let unnamed_selector = PathBuf::from(".")
+            .join(PathBuf::from("apps/web"))
+            .into_os_string();
         #[cfg(not(windows))]
         let unnamed_selector = OsString::from("./apps/web");
 
